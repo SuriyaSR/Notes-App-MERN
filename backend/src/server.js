4d445f5160406a9c4048fbbdbc4b77b50a884fba
SuriyaSR <suriyasun19@gmail.com>
2025-08-04 11:22:05 +0530

    Added Middleware & Rate Limiting functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,30 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
-
 // Middleware
-app.use(express.json());
+app.use(express.json()); // Parse JSON bodies
+app.use(rateLimiter);
+
+//simple custom middleware to log requests
+// app.use((req, res, next) => {
+//   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+//   next();
+// })
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
+  process.exit(1); // Exit the process if connection fails
 });
